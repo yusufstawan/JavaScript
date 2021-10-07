2721b045b0c0d2ce1a4bf2b32a8c76e15281fd69
Yusuf Setiyawan <yusufsetiyawan45@gmail.com>
2021-10-07 20:20:31 +0700

    07-10-2021

---
+++ Javascript DOM/js/02-script.js
@@ -0,0 +1,13 @@
+// const judul = document.getElementById('judul');
+// judul.innerHTML = 'Yusuf Setiyawan';
+
+// const judul = document.querySelector('#judul');
+// judul.style.color = 'lightblue';
+// judul.style.backgroundColor = 'salmon';
+
+
+// const judul = document.getElementsByTagName('h1')[0];
+// judul.setAttribute('name', 'sandika');
+
+
+
